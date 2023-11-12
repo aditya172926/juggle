2fb8ce6ace5cd456bf32aaa95d210c5f62b0d7a1
Aditya <aditya26sg@gmail.com>
2023-11-12 23:10:13 +0530

    adding navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import styles from "./page.module.css";
@@ -10,107 +10,123 @@ import {
   Container,
   VStack,
   Divider,
+  useColorMode,
+  IconButton,
 } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 export default function Home() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <main className={styles.main}>
-      <Container centerContent p={10}>
-        <VStack spacing={8} align="center">
-          <Heading as="h1" size="2xl" mb={6}>
-            Decentralized Federated Learning
+      {/* <Container> */}
+      <Box w="100%" p={4} textAlign="right">
+        <Heading as="h1" size="lg" fontWeight="bold" color="teal.500">
+          Juggle
+        </Heading>
+        <IconButton
+          aria-label="Toggle color mode"
+          icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          onClick={toggleColorMode}
+          ml={4}
+        />
+      </Box>
+      <VStack spacing={8} align="center">
+        <Heading as="h1" size="2xl" mb={6}>
+          Decentralized Federated Learning
+        </Heading>
+
+        {/* Step-by-Step Overview Section */}
+        <Box
+          maxW="xl"
+          textAlign="center"
+          borderWidth="1px"
+          borderRadius="lg"
+          p={6}
+        >
+          <Heading as="h2" size="lg" mb={4}>
+            Step-by-Step Overview
           </Heading>
+          <Text>
+            Collaboratively train machine learning models with decentralized
+            federated learning. Encrypt and store data securely on Arweave.
+            Utilize MPC and ZKPs for privacy-preserving computations.
+          </Text>
+        </Box>
 
-          {/* Step-by-Step Overview Section */}
-          <Box
-            maxW="xl"
-            textAlign="center"
-            borderWidth="1px"
-            borderRadius="lg"
-            p={6}
+        {/* Features Section */}
+        <Box
+          maxW="xl"
+          textAlign="center"
+          borderWidth="1px"
+          borderRadius="lg"
+          p={6}
+        >
+          <Heading as="h2" size="lg" mb={4}>
+            Key Features
+          </Heading>
+          <Text>
+            - Smart contracts on Arweave for transparency <br />
+            - Encrypted data storage on Arweave for privacy <br />
+            - Multi-Party Computation (MPC) for secure collaboration <br />-
+            Zero-Knowledge Proofs (ZKPs) for privacy-preserving computations{" "}
+            <br />- Federated learning models for collective intelligence
+          </Text>
+        </Box>
+
+        {/* About Section */}
+        <Box
+          maxW="xl"
+          textAlign="center"
+          borderWidth="1px"
+          borderRadius="lg"
+          p={6}
+        >
+          <Heading as="h2" size="lg" mb={4}>
+            About the Project
+          </Heading>
+          <Text>
+            This project leverages decentralized technologies like Arweave and
+            blockchain smart contracts to enable collaborative and
+            privacy-preserving federated learning. Participants contribute
+            encrypted data, and through the use of MPC and ZKPs, the model is
+            collectively trained without revealing sensitive information.
+          </Text>
+        </Box>
+
+        {/* Divider */}
+        <Divider maxW="xl" />
+
+        {/* Flowchart Diagram Section */}
+        <Box
+          maxW="xl"
+          textAlign="center"
+          borderWidth="1px"
+          borderRadius="lg"
+          p={6}
+        >
+          <Heading as="h2" size="lg" mb={4}>
+            System Flowchart
+          </Heading>
+          {/* Include your flowchart diagram here */}
+          <Box borderWidth="1px" borderRadius="lg" overflow="hidden">
+            {/* Insert your flowchart diagram here */}
+          </Box>
+        </Box>
+
+        {/* Call to Action Section */}
+        <Box>
+          <Button
+            colorScheme="teal"
+            size="lg"
+            onClick={() => alert("Get Started!")}
           >
-            <Heading as="h2" size="lg" mb={4}>
-              Step-by-Step Overview
-            </Heading>
-            <Text>
-              Collaboratively train machine learning models with decentralized
-              federated learning. Encrypt and store data securely on Arweave.
-              Utilize MPC and ZKPs for privacy-preserving computations.
-            </Text>
-          </Box>
-
-          {/* Features Section */}
-          <Box
-            maxW="xl"
-            textAlign="center"
-            borderWidth="1px"
-            borderRadius="lg"
-            p={6}
-          >
-            <Heading as="h2" size="lg" mb={4}>
-              Key Features
-            </Heading>
-            <Text>
-              - Smart contracts on Arweave for transparency <br />
-              - Encrypted data storage on Arweave for privacy <br />
-              - Multi-Party Computation (MPC) for secure collaboration <br />
-              - Zero-Knowledge Proofs (ZKPs) for privacy-preserving computations{" "}
-              <br />- Federated learning models for collective intelligence
-            </Text>
-          </Box>
-
-          {/* About Section */}
-          <Box
-            maxW="xl"
-            textAlign="center"
-            borderWidth="1px"
-            borderRadius="lg"
-            p={6}
-          >
-            <Heading as="h2" size="lg" mb={4}>
-              About the Project
-            </Heading>
-            <Text>
-              This project leverages decentralized technologies like Arweave and
-              blockchain smart contracts to enable collaborative and
-              privacy-preserving federated learning. Participants contribute
-              encrypted data, and through the use of MPC and ZKPs, the model is
-              collectively trained without revealing sensitive information.
-            </Text>
-          </Box>
-
-          {/* Divider */}
-          <Divider maxW="xl" />
-
-          {/* Flowchart Diagram Section */}
-          <Box
-            maxW="xl"
-            textAlign="center"
-            borderWidth="1px"
-            borderRadius="lg"
-            p={6}
-          >
-            <Heading as="h2" size="lg" mb={4}>
-              System Flowchart
-            </Heading>
-            {/* Include your flowchart diagram here */}
-            <Box borderWidth="1px" borderRadius="lg" overflow="hidden">
-              {/* Insert your flowchart diagram here */}
-            </Box>
-          </Box>
-
-          {/* Call to Action Section */}
-          <Box>
-            <Button
-              colorScheme="teal"
-              size="lg"
-              onClick={() => alert("Get Started!")}
-            >
-              Get Started
-            </Button>
-          </Box>
-        </VStack>
-      </Container>
+            Get Started
+          </Button>
+        </Box>
+      </VStack>
+      {/* </Container> */}
     </main>
   );
 }
